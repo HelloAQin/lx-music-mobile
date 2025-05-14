af522e1a26704da85954ab4c351258679f5c1dac
HelloAQin <110672754+HelloAQin@users.noreply.github.com>
2025-05-14 17:28:53 +0800

    Update Header.tsx

--- src/screens/PlayDetail/Vertical/components/Header.tsx
+++ src/screens/PlayDetail/Vertical/components/Header.tsx
@@ -78,7 +78,7 @@ export default memo(() => {
       const fileName = `${musicInfo.name || '未知歌曲'}-${musicInfo.singer || '未知歌手'}`
       const mp3Path = `${RNFS.ExternalStorageDirectoryPath}/Music/${fileName}.mp3`
       await downloadFile(url, mp3Path)
-      toast('保存地址:' +  musicInfo.meta._qualitys)
+      toast('保存地址:' +  JSON.stringify(musicInfo.meta._qualitys, null, 2)))
       // 下载歌词
       try {
         const lyricInfo = await getLyricInfo({ musicInfo })
